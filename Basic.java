1411942051bbfdd3ab02daa25256b5fe058b0745
NiranjanRongala <niranjan.rongala07@gmail.com>
2023-08-07 09:54:12 +0530

    first commit1

---
+++ Basic.java
@@ -0,0 +1,45 @@
+package first;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        //	System.out.println(getSum(10, 20, 30));
+
+        //System.out.println(getSum(100, 200));
+
+        //}
+
+
+        // TODO Auto-generated method stub
+        //	Scanner sc=new Scanner(System.in);
+        int s=10;
+        Character c='s';
+        Integer a=0b1010;
+        float z=2.8f;
+        int i=0|0;
+        byte w=10,oo=6;
+        int mm[];
+        mm=new int[s];
+        int cc[]= {1,2,5,99,0,88};
+        Arrays.sort(cc);
+        Arrays.binarySearch(cc,5);
+        for(int j=0;j<mm.length;j++) {
+            // mm[j]=sc.nextInt();
+        }
+        for(int vv:cc)
+            System.out.println(vv);
+
+
+
+        System.out.println(c+" "+a+"\'"+i+" "+w);
+        System.out.printf("%-10d %d",i,s);
+
+
+    }
+
+}
+
+
